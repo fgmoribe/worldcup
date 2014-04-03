33a57c51cf665a28c7f39e013f44431132b1c8a9
Fernando Moribe <fgmoribe@gmail.com>
2014-04-03 17:04:31 -0300

    creation of seeds and adjusts on json format for games

--- app/views/games/index.json.jbuilder
+++ app/views/games/index.json.jbuilder
@@ -1,21 +1,25 @@
 json.games @games do |game|
 
 	json.id game.id
+	json.game_number game.game_number
 	json.team1 do
+		json.team_number game.team1.team_number
 		json.name game.team1.name
 		json.portuguese_name game.team1.portuguese_name
 	end
 	json.goals1 game.goals1
 	json.team2 do
+		json.team_number game.team2.team_number
 		json.name game.team2.name
 		json.portuguese_name game.team2.portuguese_name
 	end
 	json.goals2 game.goals2
 	json.venue do
-		#json.venue_id game.venue.id
-		#json.name game.venue.name
+		json.venue_number game.venue.venue_number
+		json.name game.venue.name
 	end
-	json.venue game.venue
+	json.group_number game.group.group_number
+	json.grop_name game.group.name
 	json.date game.date
 	json.time game.time
 
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+groupA = Group.create(group_number: 1, name: 'A')
+
+brazil = Team.create(team_number: 1, name: 'Brazil', portuguese_name: 'Brasil', group: groupA)
+croatia = Team.create(team_number: 2, name: 'Croatia', portuguese_name: 'Croacia', group: groupA)
+mexico = Team.create(team_number: 3, name: 'Mexico', portuguese_name: 'Mexico', group: groupA)
+cameroon = Team.create(team_number: 4, name: 'Cameroon', portuguese_name: 'Camarões', group: groupA)
+
+sp = Venue.create(venue_number: 1, name: 'São Paulo')
+natal = Venue.create(venue_number: 2, name: 'Natal')
+fortaleza = Venue.create(venue_number: 3, name: 'Fortaleza')
+manaus = Venue.create(venue_number: 4, name: 'Manaus')
+brasilia = Venue.create(venue_number: 5, name: 'Brasilia')
+recife = Venue.create(venue_number: 6, name: 'Recife')
+
+
+Game.create(game_number: 1, team1: brazil, team2: croatia, venue:sp, date: '12/06', time:'17:00', group: groupA)
+Game.create(game_number: 2, team1: mexico, team2: cameroon, venue:natal, date: '13/06', time:'13:00', group: groupA)
+Game.create(game_number: 3, team1: brazil, team2: mexico, venue:fortaleza, date: '17/06', time:'16:00', group: groupA)
+Game.create(game_number: 4, team1: cameroon, team2: cameroon, venue:natal, date: '13/06', time:'13:00', group: groupA)
