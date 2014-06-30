41c6776aa6b7f3b73a08f1a934f767e404f6accf
Fernando Moribe <fgmoribe@gmail.com>
2014-06-30 14:56:35 -0300

    game53

--- db/seeds.rb
+++ db/seeds.rb
@@ -641,6 +641,7 @@ game49.update(goals1: 1, goals2: 1, penalty1: 3, penalty2:2)
 game50.update(goals1: 2, goals2: 0)
 game51.update(goals1: 2, goals2: 1)
 game52.update(goals1: 1, goals2: 1, penalty1: 5, penalty2: 3)
+game53.update(goals1: 2, goals2: 0)
 
 ##### GAMES - QUARTER-FINALS
 Game.create(game_number: 57, 
@@ -651,7 +652,7 @@ Game.create(game_number: 57,
 			time: '17:00', 
 			group: group_quarter)
 Game.create(game_number: 58, 
-			team1: tbd, 
+			team1: france, 
 			team2: tbd, 
 			venue: rio, 
 			date: '04/07', 
